e03d5851b162eef13e61710f1c17633fab9e226a
Marcolux <mccolucci1@gmail.com>
2023-12-22 13:59:22 -0600

    Added coming soon page

--- mareland/index/components/coming-soon/coming_soon.markup.js
+++ mareland/index/components/coming-soon/coming_soon.markup.js
@@ -4,10 +4,22 @@ class ComingSoonPage extends HTMLElement {
         super()
 
         this.innerHTML = `
-            <div class="coming-soon-container">
-                <h1>Coming Soon</h1>
-                <p>We're working hard to bring you this content. Stay tuned!</p>
+        <div class="coming-soon-container d-flex flex-column align-items-center">
+            <h1 class="mb-5 comingSoonTitle">MaReLand</h1>
+            <div class="col-12 d-flex justify-content-center">
+            <img src="./mareland/images/icon/whiteRoundLogo.png" class="logoComingSoon" alt="mareland">
             </div>
+            <div class="d-flex col-12 align-items-end justify-content-center mt-5">
+                <h1 class="mb-0 text-bold">Coming Soon</h1>
+                <div class="dots-loading ml-2 mb-1">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        
+        
+        </div>
         `
     }
 }
