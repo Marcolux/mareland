89c5cfb15b606a618cacb2919a5c8b87610a8925
Marcolux <mccolucci1@gmail.com>
2024-02-08 11:05:41 -0600

    Added images and hamb menu

--- mareland/script/main.js
+++ mareland/script/main.js
@@ -1,2 +1,13 @@
 console.log('hello world');
 console.log('all the time');
+// import '../index/components/coming-soon/coming_soon.markup'
+var hambMenu = document.querySelector('.hamburger-menu');
+var navMenu = document.getElementById('navMenu');
+var navBar = document.querySelector('.navBar');
+hambMenu.addEventListener('click', function () {
+    navMenu.classList.toggle('show');
+    navMenu.classList.toggle('hide', !navMenu.classList.contains('show'));
+    hambMenu.classList.toggle('openHam');
+    navBar.classList.toggle('expand');
+});
+console.log("tsc running 2");
--- mareland/script/main.ts
+++ mareland/script/main.ts
@@ -1,3 +1,15 @@
 console.log('hello world')
 console.log('all the time')
-import '../index/components/coming-soon/coming_soon.markup'
+// import '../index/components/coming-soon/coming_soon.markup'
+const hambMenu = document.querySelector('.hamburger-menu') as HTMLElement
+const navMenu = document.getElementById('navMenu') as HTMLElement
+const navBar = document.querySelector('.navBar') as HTMLElement
+
+hambMenu.addEventListener('click', function() {
+    navMenu.classList.toggle('show')
+    navMenu.classList.toggle('hide',!navMenu.classList.contains('show'))
+    hambMenu.classList.toggle('openHam') 
+    navBar.classList.toggle('expand')
+})
+
+console.log("tsc running 2")
