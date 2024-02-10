30b357b61f5369701a28216d8c64684cd43f5a9f
Marcolux <mccolucci1@gmail.com>
2024-02-09 20:15:55 -0600

    Completed landing page

--- mareland/script/main.js
+++ mareland/script/main.js
@@ -1,13 +1,99 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 console.log('hello world');
 console.log('all the time');
 // import '../index/components/coming-soon/coming_soon.markup'
 var hambMenu = document.querySelector('.hamburger-menu');
 var navMenu = document.getElementById('navMenu');
 var navBar = document.querySelector('.navBar');
+var spanToBreak = document.querySelectorAll('.brSm');
 hambMenu.addEventListener('click', function () {
     navMenu.classList.toggle('show');
     navMenu.classList.toggle('hide', !navMenu.classList.contains('show'));
     hambMenu.classList.toggle('openHam');
     navBar.classList.toggle('expand');
 });
-console.log("tsc running 2");
+var navBarAdjToScreen = function () {
+    if (window.innerWidth < 505) {
+        navBar.classList.add('mobileView');
+        navBar.classList.remove('biggerScreen');
+        spanToBreak.forEach(function (el) {
+            if (!el.firstChild || el.firstChild.nodeName !== 'BR') {
+                var brEl = document.createElement('br');
+                el.prepend(brEl);
+            }
+        });
+    }
+    else {
+        navBar.classList.remove('mobileView');
+        navBar.classList.add('biggerScreen');
+        spanToBreak.forEach(function (el) {
+            if (el.firstChild && el.firstChild.nodeName === 'BR') {
+                el.removeChild(el.firstChild);
+            }
+        });
+    }
+};
+var selector = document.getElementById('rclmtnPicSelector');
+var display = document.getElementById('rclmtnBigPic');
+var allTheBigPic = display.querySelectorAll('.bigPicShow');
+var allTheSmPic = selector.querySelectorAll('.smPicSelector');
+var carouselLogic = function (selector, display) {
+    selector.forEach(function (smPic, index) {
+        smPic.addEventListener('click', function () {
+            selector.forEach(function (pic) { return pic.classList.remove('picClicked'); });
+            display.forEach(function (pic) { return pic.classList.add('hide'); });
+            smPic.classList.add('picClicked');
+            if (display[index]) {
+                display[index].classList.remove('hide');
+            }
+        });
+    });
+    var leftButton = document.querySelector('.scroll-button.left');
+    var rightButton = document.querySelector('.scroll-button.right');
+    var pictureContainer = document.querySelector('#scroller');
+    var currentIndex = 0; // Keep track of the current index
+    leftButton.addEventListener('click', function () {
+        if (currentIndex > 0) {
+            currentIndex--; // Move to the previous picture
+            scrollByPicWidth('left');
+            rightButton.classList.remove('hide');
+        }
+        if (currentIndex < 1) {
+            leftButton.classList.add('hide');
+        }
+    });
+    rightButton.addEventListener('click', function () {
+        if (currentIndex < 3) {
+            currentIndex++; // Move to the next picture
+            scrollByPicWidth('right');
+        }
+        if (currentIndex > 0) {
+            leftButton.classList.remove('hide');
+        }
+        if (currentIndex > 2) {
+            rightButton.classList.add('hide');
+        }
+    });
+    function scrollByPicWidth(direction) {
+        var width = selector[1].offsetWidth;
+        console.log(width);
+        if (direction === 'right') {
+            pictureContainer.scrollLeft += width;
+        }
+        else {
+            pictureContainer.scrollLeft -= width - 2;
+        }
+    }
+};
+carouselLogic(__spreadArray([], allTheSmPic, true), __spreadArray([], allTheBigPic, true));
+window.addEventListener('resize', navBarAdjToScreen);
+navBarAdjToScreen();
+console.log("tsc running 9");
--- mareland/script/main.ts
+++ mareland/script/main.ts
@@ -4,6 +4,7 @@ console.log('all the time')
 const hambMenu = document.querySelector('.hamburger-menu') as HTMLElement
 const navMenu = document.getElementById('navMenu') as HTMLElement
 const navBar = document.querySelector('.navBar') as HTMLElement
+const spanToBreak = document.querySelectorAll('.brSm') as NodeListOf<HTMLElement>
 
 hambMenu.addEventListener('click', function() {
     navMenu.classList.toggle('show')
@@ -12,4 +13,87 @@ hambMenu.addEventListener('click', function() {
     navBar.classList.toggle('expand')
 })
 
-console.log("tsc running 2")
+const navBarAdjToScreen = () => {
+    if (window.innerWidth < 505) {
+        navBar.classList.add('mobileView')
+        navBar.classList.remove('biggerScreen')
+        spanToBreak.forEach(el => {
+            if (!el.firstChild || el.firstChild.nodeName !== 'BR') {
+                const brEl = document.createElement('br')
+                el.prepend(brEl)
+            }
+        })
+    } else {
+        navBar.classList.remove('mobileView')
+        navBar.classList.add('biggerScreen')
+        spanToBreak.forEach(el => {
+            if (el.firstChild && el.firstChild.nodeName === 'BR') {
+                el.removeChild(el.firstChild)
+            }
+        })
+    }
+}
+const selector = document.getElementById('rclmtnPicSelector') as HTMLDivElement
+const display = document.getElementById('rclmtnBigPic') as HTMLDivElement
+const allTheBigPic = display.querySelectorAll('.bigPicShow') as NodeListOf<HTMLImageElement>
+const allTheSmPic = selector.querySelectorAll('.smPicSelector') as NodeListOf<HTMLImageElement>
+
+const carouselLogic = (selector,display) => {
+    selector.forEach((smPic, index) => {
+
+        smPic.addEventListener('click', () => {
+            selector.forEach(pic => pic.classList.remove('picClicked'))
+            display.forEach(pic => pic.classList.add('hide'))
+
+            smPic.classList.add('picClicked') 
+            if (display[index]) {
+                display[index].classList.remove('hide')
+            }
+        })
+    })
+
+    const leftButton = document.querySelector('.scroll-button.left') as HTMLElement
+    const rightButton = document.querySelector('.scroll-button.right') as HTMLElement
+    const pictureContainer = document.querySelector('#scroller') as HTMLDivElement
+    let currentIndex = 0 // Keep track of the current index
+
+    leftButton.addEventListener('click', function() {
+        if (currentIndex > 0) {
+            currentIndex-- // Move to the previous picture
+            scrollByPicWidth('left')
+            rightButton.classList.remove('hide')
+        } 
+        if (currentIndex < 1) {
+            leftButton.classList.add('hide')
+        }
+    })
+    
+    rightButton.addEventListener('click', function() {
+        if (currentIndex < 3) {
+            currentIndex++ // Move to the next picture
+            scrollByPicWidth('right')
+        } 
+        if (currentIndex > 0) {
+            leftButton.classList.remove('hide')
+        } 
+        if (currentIndex > 2) {
+            rightButton.classList.add('hide')
+        }
+    })
+
+    function scrollByPicWidth(direction) {
+        const width = selector[1].offsetWidth
+        console.log(width)
+        if (direction === 'right') {
+            pictureContainer.scrollLeft += width;
+        } else {
+            pictureContainer.scrollLeft -= width - 2;
+        }
+    }
+
+}
+
+carouselLogic([...allTheSmPic],[...allTheBigPic])
+window.addEventListener('resize',navBarAdjToScreen)
+navBarAdjToScreen()
+console.log("tsc running 9")
